84082d453f517a0091289e022e8d1640b4de2902
Christian Pehle <christian.pehle@gmail.com>
2022-07-27 15:47:42 +0200

    feat: jax jit compatible solver
    Change-Id: Ie6e350d55b08da00ad4ca2b9f7b7b4b3f7de4ec5

--- jaxsnn/linear_solver.py
+++ jaxsnn/linear_solver.py
@@ -1,7 +1,34 @@
-def hines_solver(d, u, b, p):
+from re import A
+import jax.numpy as jnp
+import jax.lax as lax
+from functools import partial
+
+
+def tree_to_matrix(d, u, p):
   """
-  d : diagonal of the matrix to be solved
-  u : upper part with indices 0 .. N
+  Auxiliary function, which turns a 'tree' matrix into
+  a dense matrix. This is mostly convenient for testing.
+  """
+  N = d.shape[0]
+  a = jnp.diag(d, 0)
+
+  for i in range(1,N):
+    a = a.at[p[i],i].set(u[i-1])
+    a = a.at[i, p[i]].set(u[i-1])
+  return a
+
+
+def tree_matmul(d, u, p, b):
+  """
+  Multiply a 'tree' matrix with a vector.
+  """
+  # TODO: Dummy implementation
+  m = tree_to_matrix(d,u,p)
+  return jnp.dot(m, b)
+
+
+def hines_solver(d, u, p, b):
+  """
   """
   N = d.shape[0]
 
@@ -16,3 +43,16 @@ def hines_solver(d, u, b, p):
     b = b.at[i].set((b[i] - u[p[i]] * b[p[i]]) / d[i])
 
   return b
+
+
+def tree_solve(d, u, p, b):
+  """
+  A solver for 'tree' matrices, which is compatible with the jax tracer.
+  """
+  solver = partial(
+    lax.custom_linear_solve,
+    lambda x: tree_matmul(d, u, p, x),
+    solve = lambda _, x: hines_solver(d, u, p, x),
+    symmetric = True)
+
+  return solver(b)
--- jaxsnn/linear_solver_test.py
+++ jaxsnn/linear_solver_test.py
@@ -4,15 +4,15 @@ import jax.numpy as jnp
 import numpy as onp
 
 def test_hines_solver():
+    # TODO: This only tests the tri-diagonal case.
     N = 10
     d = 2*onp.random.randn(N)
     u = onp.random.randn(N-1)
     b = onp.random.randn(N)
     p = onp.arange(-1,N,1)
-    p = onp.arange(-1,N,1)
     a = onp.diag(d, 0) + onp.diag(u, 1) + onp.diag(u, -1)
 
-    x = hines_solver(jnp.array(d), jnp.array(u), jnp.array(b), jnp.array(p))
+    x = hines_solver(jnp.array(d), jnp.array(u), jnp.array(p),  jnp.array(b))
     x_ = jnp.linalg.solve(a, b)
 
     # TODO: This is a rather liberal error tolerance...
