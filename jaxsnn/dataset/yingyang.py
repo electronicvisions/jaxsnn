fix: use log softmax, change membrane potential
Change-Id: Ie5ff8020770fa42b526676a8e3b24937e7a2edd3

jaxsnn/dataset/yingyang.py
@@ -1,8 +1,5 @@
-import time
-
 import jax.numpy as jnp
-from jax import random
-from jax import vmap
+from jax import random, vmap
 
 
 def outside_circle(x: float, y: float, r_big) -> bool:
@@ -17,7 +14,7 @@ def dist_to_left_dot(x: int, y: int, r_big) -> float:
     return jnp.sqrt((x - 0.5 * r_big) ** 2 + (y - r_big) ** 2)
 
 
-def get_class(coords, r_big: float, r_small: float) -> jnp.DeviceArray:
+def get_class(coords, r_big: float, r_small: float):
     # # equations inspired by
     # # https://link.springer.com/content/pdf/10.1007/11564126_19.pdf
     # outside of circle is a different class
@@ -29,7 +26,11 @@ def get_class(coords, r_big: float, r_small: float) -> jnp.DeviceArray:
     criterion3 = jnp.logical_and(y > r_big, d_right > 0.5 * r_big)
     is_yin = jnp.logical_or(jnp.logical_or(criterion1, criterion2), criterion3)
     is_circles = jnp.logical_or(d_right < r_small, d_left < r_small)
-    return is_circles.astype(int) * 2 + jnp.invert(is_circles).astype(int) * is_yin.astype(int) + outside_circle(x, y, r_big) * 10
+    return (
+        is_circles.astype(int) * 2
+        + jnp.invert(is_circles).astype(int) * is_yin.astype(int)
+        + outside_circle(x, y, r_big) * 10
+    )
 
 
 get_class_batched = vmap(get_class, in_axes=(0, None, None))
@@ -65,21 +66,21 @@ class YinYangDataset:
         key, subkey = random.split(key)
 
         # on average we need arount 7 tries for one sample
-        coords = random.uniform(
-            key, (size * 10 + 100, 2)) * 2.0 * self.r_big
+        coords = random.uniform(key, (size * 10 + 100, 2)) * 2.0 * self.r_big
 
         classes = get_class_batched(coords, self.r_big, self.r_small)
 
-        n_per_class = jnp.array([size // 3, size // 3, size - 2 * size // 3])
+        n_per_class = [size // 3, size // 3, size - 2 * size // 3]
         idx = jnp.concatenate(
-            [jnp.where(classes == i)[0][:n] for i, n in enumerate(n_per_class)])
+            [jnp.where(classes == i)[0][:n] for i, n in enumerate(n_per_class)]
+        )
 
         idx = random.permutation(subkey, idx, axis=0)
-        self.vals = jnp.hstack((coords, 1 - coords))
+        self.vals = jnp.hstack((coords[idx], 1 - coords[idx]))
         self.classes = classes[idx]
 
     def __getitem__(self, index: int):
-        return (self.vals[index], self.classes[index])
+        return (self.vals[index], self.classes[index])  # type: ignore
 
     def __len__(self):
         return len(self.classes)

jaxsnn/functional/encode.py
@@ -1,6 +1,8 @@
 import jax.numpy as jnp
-from .lif import lif_current_encoder
+from jax import jit
 from jax.lax import scan
+
+from .lif import lif_current_encoder
 
 
 def one_hot(x, k, dtype=jnp.float32):
@@ -9,9 +11,9 @@ def one_hot(x, k, dtype=jnp.float32):
 
 
 def constant_current_lif_encode(
-    input_current: jnp.DeviceArray,
+    input_current,
     seq_length: int,
-) -> jnp.DeviceArray:
+):
     """
     Encodes input currents as fixed (constant) voltage currents, and simulates the spikes that
     occur during a number of timesteps/iterations (seq_length).
@@ -40,11 +42,11 @@ def constant_current_lif_encode(
 
 
 def spatio_temporal_encode(
-    input_values: jnp.DeviceArray,
+    input_values,
     seq_length: int,
     t_late: float,
     dt: float,
-) -> jnp.DeviceArray:
+):
     """
     Encodes n-dimensional input coordinates with range [0, 1], and simulates the spikes that
     occur during a number of timesteps/iterations (seq_length).
@@ -68,12 +70,12 @@ def spatio_temporal_encode(
         A tensor with an extra dimension of size `seq_length` containing spikes (1) or no spikes (0).
     """
     if len(input_values.shape) > 2:
-        raise ValueError(
-            "Tensor with input values must be one or two dimensional")
+        raise ValueError("Tensor with input values must be one or two dimensional")
 
     idx = (input_values * t_late / dt).round().astype(int)
     idx = jnp.clip(idx, 0, seq_length)
-    return jnp.eye(seq_length)[:, idx]
+    encoded = jnp.eye(seq_length)[:, idx]
+    return encoded
 
 
 if __name__ == "__main__":
@@ -81,4 +83,3 @@ if __name__ == "__main__":
     # value = spatio_temporal_encode(data, 10)
 
     data = jnp.array([0.5, 0.8, 0.5, 0.2])
-    value = spatio_temporal_encode(data, 20)

jaxsnn/functional/leaky_integrator.py
@@ -1,6 +1,8 @@
-import jax.numpy as jnp
 from typing import NamedTuple, Tuple
-from jax import random
+
+import jax.numpy as jnp
+from jax import jit, random
+from jax.lax import scan
 
 
 class LIState(NamedTuple):
@@ -29,13 +31,14 @@ class LIParameters(NamedTuple):
     v_leak: jnp.DeviceArray = jnp.array(0.0)
 
 
+@jit
 def li_feed_forward_step(
-    state: LIState,
-    input_weights: jnp.DeviceArray,
+    init,
     spikes: jnp.DeviceArray,
     p: LIParameters = LIParameters(),
     dt: float = 0.001,
-) -> Tuple[jnp.DeviceArray, LIState]:
+):
+    state, input_weights = init
     # compute current jumps
     i_jump = state.i + jnp.matmul(spikes, input_weights)
     # compute voltage updates
@@ -46,10 +49,17 @@ def li_feed_forward_step(
     di = -dt * p.tau_syn_inv * i_jump
     i_decayed = i_jump + di
 
-    return LIState(v_new, i_decayed), v_new
+    return (LIState(v_new, i_decayed), input_weights), v_new
 
 
-def li_init_weights(key: random.KeyArray, input_size: float, size: float, scale: float = 1e-2) -> Tuple[jnp.DeviceArray]:
+@jit
+def li_integrate(init, spikes):
+    return scan(li_feed_forward_step, init, spikes)
+
+
+def li_init_weights(
+    key: random.KeyArray, input_size: int, size: int, scale: float = 1e-2
+) -> Tuple[jnp.DeviceArray]:
     """Randomly initialize weights for a li layer
 
     Args:
@@ -60,8 +70,8 @@ def li_init_weights(key: random.KeyArray, input_size: float, size: float, scale:
     Returns:
         Tuple[jnp.DeviceArray]: Randomly initialized weights
     """
-    return (scale * random.normal(key, (input_size, size)), )
+    return (scale * random.normal(key, (input_size, size)),)
 
 
-def li_init_state(size: int) -> LIState:
+def li_init_state(size: Tuple[int, int]) -> LIState:
     return LIState(jnp.zeros(size), jnp.zeros(size))

jaxsnn/functional/lif.py
@@ -1,10 +1,8 @@
-from functools import partial
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 
 import jax.numpy as jnp
-from jax import custom_vjp, grad, jit, random, vmap
+from jax import custom_vjp, jit, random
 from jax.lax import scan
-from typing import Tuple
 
 
 class LIFState(NamedTuple):
@@ -17,6 +15,7 @@ class LIFState(NamedTuple):
         input_weights (jnp.DeviceArray): input weights
         recurrent_weights (jnp.DeviceArray): recurrentweights
     """
+
     z: jnp.DeviceArray
     v: jnp.DeviceArray
     i: jnp.DeviceArray
@@ -41,7 +40,7 @@ class LIFParameters(NamedTuple):
     tau_syn_inv: jnp.DeviceArray = jnp.array(1.0 / 5e-3)
     tau_mem_inv: jnp.DeviceArray = jnp.array(1.0 / 1e-2)
     v_leak: jnp.DeviceArray = jnp.array(0.0)
-    v_th: jnp.DeviceArray = jnp.array(1.0)
+    v_th: jnp.DeviceArray = jnp.array(0.5)
     v_reset: jnp.DeviceArray = jnp.array(0.0)
 
 
@@ -55,7 +54,7 @@ def heaviside_fwd(x):
 
 
 def heaviside_bwd(res, g):
-    x, = res
+    (x,) = res
     grad = g / (100.0 * jnp.abs(x) + 1.0) ** 2
     return (grad,)
 
@@ -64,7 +63,7 @@ heaviside.defvjp(heaviside_fwd, heaviside_bwd)
 
 
 def lif_current_encoder(
-    voltage: jnp.DeviceArray,
+    voltage,
     input_current: jnp.DeviceArray,
     p: LIFParameters = LIFParameters(),
     dt: float = 0.001,
@@ -92,14 +91,14 @@ def lif_current_encoder(
     return voltage, z
 
 
+@jit
 def lif_step(
-    state: LIFState,
-    input_weights: jnp.DeviceArray,
-    recurrent_weights: jnp.DeviceArray,
-    spikes: jnp.DeviceArray,
+    init,
+    spikes,
     params: LIFParameters = LIFParameters(),
     dt=0.001,
 ):
+    state, input_weights, recurrent_weights = init
     z, v, i = state
     tau_syn_inv, tau_mem_inv, v_leak, v_th, v_reset = params
 
@@ -116,20 +115,20 @@ def lif_step(
     # compute reset
     v_new = (1 - z_new) * v_decayed + z_new * v_reset
     # compute current jumps
-    i_new = (
-        i_decayed
-        + jnp.matmul(z, recurrent_weights)
-    )
+    i_new = i_decayed + jnp.matmul(z, recurrent_weights)
     i_new = i_new + jnp.matmul(spikes, input_weights)
 
-    return LIFState(z_new, v_new, i_new), z_new
+    return (LIFState(z_new, v_new, i_new), input_weights, recurrent_weights), z_new
 
 
+@jit
 def lif_integrate(init, spikes):
     return scan(lif_step, init, spikes)
 
 
-def lif_init_weights(key: random.KeyArray, input_size: float, size: float, scale: float = 1e-2) -> Tuple[jnp.DeviceArray, jnp.DeviceArray]:
+def lif_init_weights(
+    key: random.KeyArray, input_size: int, size: int, scale: float = 1e-2
+) -> Tuple[jnp.DeviceArray, jnp.DeviceArray]:
     """Randomly initialize weights and recurrent weights for a snn layer
 
     Args:
@@ -142,10 +141,9 @@ def lif_init_weights(key: random.KeyArray, input_size: float, size: float, scale
     """
     i_key, r_key = random.split(key)
     input_weights = scale * random.normal(i_key, (input_size, size))
-    recurrent_weights = scale * \
-        random.normal(r_key, (size, size))
+    recurrent_weights = scale * random.normal(r_key, (size, size))
     return input_weights, recurrent_weights
 
 
-def lif_init_state(size: Tuple[int]) -> LIFState:
+def lif_init_state(size: Tuple[int, int]) -> LIFState:
     return LIFState(jnp.zeros(size), jnp.zeros(size), jnp.zeros(size))

jaxsnn/model/snn.py
@@ -1,55 +1,65 @@
-from typing import List
+from typing import List, Tuple
 
 import jax
 import jax.numpy as jnp
-from jax import value_and_grad
+from jax import jit, value_and_grad
+from jax.lax import scan
 from jax.tree_util import tree_map
-from jaxsnn.functional.leaky_integrator import (li_feed_forward_step,
-                                                li_init_state)
-from jaxsnn.functional.lif import lif_init_state, lif_step
+from jaxsnn.functional.encode import one_hot
+from jaxsnn.functional.leaky_integrator import (
+    li_feed_forward_step,
+    li_init_state,
+    li_integrate,
+)
+from jaxsnn.functional.lif import lif_init_state, lif_integrate, lif_step
 
 
 def decode(x):
     x = jnp.max(x, 0)
-    log_p_y = jax.nn.softmax(x, axis=1)
+    log_p_y = jax.nn.log_softmax(x, axis=1)
     return log_p_y
 
 
+@jit
 def update(
     params,
-    x: jnp.DeviceArray,
-    y: jnp.DeviceArray,
-    # last_grads,
-    # momentum_mass: float,
+    last_grads,
+    state,
+    x,
+    y,
+    momentum: float,
     step_size: float,
 ):
-    loss_value, grads = value_and_grad(loss)(params, (x, y))
-    a = 4
-    return tree_map(
-        lambda p, g: p - step_size * g,  # (g + momentum_mass * lg),
+    # at the moment we do not take grad of state or grad of topology
+    (loss_value, recording), grads = value_and_grad(loss, has_aux=True)(
+        params, state, (x, y)
+    )
+    params = tree_map(
+        lambda p, g, lg: p - step_size * (g + momentum * lg),
         params,
         grads,
-        # last_grads,
-    ), loss_value
+        last_grads,
+    )
+    return params, grads, loss_value, recording
 
 
-def loss(params: List, batch):
+def loss(params, state, batch) -> Tuple[float, jnp.DeviceArray]:
     inputs, targets = batch
-    preds = forward(params, inputs)
-    return -jnp.mean(jnp.sum(targets * preds, axis=1))
+    preds, recording = forward(params, state, inputs)
+    return -jnp.mean(jnp.sum(targets * preds, axis=1)), recording
 
 
-# do multiple time steps, later convert to one time step + scan function
-def forward(params: List, input_values: jnp.DeviceArray):
-    batch_size = input_values.shape[1]
-    states = [lif_init_state((batch_size, 50)), li_init_state((batch_size, 3))]
-    voltages = []
-    lif_state, li_state = states
-    for ts in range(input_values.shape[0]):
-        lif_state, z = lif_step(lif_state, *params[0], input_values[ts])
-        if jnp.count_nonzero(z) > 0:
-            print(f"Spike count: {jnp.count_nonzero(z)}")
-        li_state, voltage = li_feed_forward_step(li_state, *params[1], z)
-        voltages.append(voltage)
-    log_p_y = decode(jnp.array(voltages))
-    return log_p_y
+def accuracy_and_loss(params, state, batch):
+    inputs, targets = batch
+    preds, _ = forward(params, state, inputs)
+    correct = (jnp.argmax(preds, axis=1) == jnp.argmax(targets, axis=1)).sum()
+    accuracy = correct / len(targets)
+    loss = -jnp.mean(jnp.sum(targets * preds, axis=1))
+    return accuracy, loss
+
+
+def forward(params, states, input_values) -> Tuple:
+    _, spikes = lif_integrate((states[0], *params[0]), input_values)
+    _, voltages = li_integrate((states[1], *params[1]), spikes)
+    log_p_y = decode(voltages)
+    return log_p_y, (voltages, spikes)

jaxsnn/tasks/yinyang.py
@@ -1,12 +1,15 @@
+import copy
+import time
 from dataclasses import dataclass
 
 import jax.numpy as jnp
 from jax import random
+from jax.tree_util import tree_map
 from jaxsnn.dataset.yingyang import YinYangDataset
 from jaxsnn.functional.encode import one_hot, spatio_temporal_encode
-from jaxsnn.functional.leaky_integrator import li_init_weights
-from jaxsnn.functional.lif import LIFParameters, lif_init_weights
-from jaxsnn.model.snn import update
+from jaxsnn.functional.leaky_integrator import li_init_state, li_init_weights
+from jaxsnn.functional.lif import LIFParameters, lif_init_state, lif_init_weights
+from jaxsnn.model.snn import accuracy_and_loss, update
 
 
 @dataclass
@@ -23,8 +26,7 @@ class Loader:
         if self.idx < len(self):
             start = self.idx * self.batch_size
             stop = (self.idx + 1) * self.batch_size
-            result = self.dataset.vals[start:
-                                       stop], self.dataset.classes[start: stop]
+            result = self.dataset.vals[start:stop], self.dataset.classes[start:stop]  # type: ignore
             self.idx += 1
             return result
         else:
@@ -35,29 +37,80 @@ class Loader:
 
 
 if __name__ == "__main__":
-    batch_size = 32
-    epochs = 5
-    step_size = 0.0005
-    T = 80
-    DT = 5e-4
-    t_late = 1.0 / LIFParameters().tau_syn_inv + 1.0 / LIFParameters().tau_mem_inv
+    batch_size = 64
+    epochs = 100
+    step_size = 0.05
+    n_classes = 3
+    hidden_features = 50
+    input_features = 4  # make this more generic
+    momentum = 0.9
 
-    def train(params, trainloader, step_size):
+    # this spatial resolution may not permit better results
+    T = 50
+    DT = 5e-4
+    t_late = 1.0 / LIFParameters().tau_syn_inv + 1.0 / LIFParameters().tau_mem_inv  # type: ignore
+
+    # TODO use scan over update for train function
+    # train = scan(update, trainloader)
+    # train()
+    def train(params, state, trainloader, step_size):
         losses = []
+        grads = tree_map(lambda p: jnp.zeros_like(p), params)
         for (input, output) in trainloader:
             input = spatio_temporal_encode(input, T, t_late, DT)
-            output = one_hot(output, 3)
-            params, loss = update(params, input, output, step_size=step_size)
+            output = one_hot(output, n_classes)
+            state = copy.deepcopy(state)
+            params, grads, loss, recording = update(
+                params, grads, state, input, output, momentum, step_size
+            )
             losses.append(loss)
-        return losses, jnp.mean(jnp.array(losses))
+        return params, losses, jnp.mean(jnp.array(losses))
+
+    # TODO vmap over accuracy and loss
+    # test = vmap(accuracy_and_loss, testloader)
+    # test()
+    def test(params, testset):
+        states = [
+            lif_init_state((len(testset), params[0][0].shape[1])),
+            li_init_state((len(testset), params[1][0].shape[1])),
+        ]
+        input = spatio_temporal_encode(testset.vals, T, t_late, DT)
+        output = one_hot(testset.classes, n_classes)
+        accuracy, loss = accuracy_and_loss(params, states, (input, output))
+        return jnp.mean(accuracy), jnp.mean(loss)
 
     key = random.PRNGKey(42)
-    dataset = YinYangDataset(key, 1000)
+    dataset = YinYangDataset(key, 6400)
 
-    params = [lif_init_weights(key, 4, 50, scale=0.5),
-              li_init_weights(key, 50, 3, scale=0.2)]
+    test_key = random.PRNGKey(10)
+    test_dataset = YinYangDataset(test_key, 1000)
+
+    # define network with states and params
+    # maybe just define forward function which we can pass to train?
+    # TODO look how this is done in flax
+    params = [
+        lif_init_weights(key, input_features, hidden_features, scale=0.5),
+        li_init_weights(key, hidden_features, n_classes, scale=0.2),
+    ]
+    states = [
+        lif_init_state((batch_size, params[0][0].shape[1])),
+        li_init_state((batch_size, params[1][0].shape[1])),
+    ]
+
+    # TODO we need adam optimizer or adaptive lr for better results
 
     for epoch in range(epochs):
-        training_losses, mean_loss = train(
-            params, Loader(dataset, batch_size), step_size)
-        print(f"Epoch: {epoch}, Loss: {mean_loss}")
+        start = time.time()
+        params, training_losses, mean_loss = train(
+            params, states, Loader(dataset, batch_size), step_size
+        )
+        train_time = time.time() - start
+        start = time.time()
+        accuracy, loss = test(params, test_dataset)
+        test_time = time.time() - start
+        print(
+            f"Epoch: {epoch}, Loss: {mean_loss}, Test accuracy: {accuracy:.2f}, Seconds: {train_time:.3f}, {test_time:.3f}"
+        )
+
+
+# test_loss, accuracy = test(model, DEVICE, test_loader, epoch, rho_a=0.5)