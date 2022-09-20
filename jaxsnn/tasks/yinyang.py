3ffafad80559e4d8dc61add23f7d39a7e9051e60
Moritz Althaus <moritzalthaus@gmx.de>
2022-09-20 09:23:39 +0200

    feat: use optimizer from  instead of
    Change-Id: I21d4001874302409a79a400d0b2e2a7d6f513425

--- jaxsnn/tasks/yinyang.py
+++ jaxsnn/tasks/yinyang.py
@@ -2,27 +2,25 @@ import time
 from functools import partial
 
 import jaxsnn
+import optax
 from jax import numpy as jnp
 from jax import random, value_and_grad
-from jax.example_libraries import optimizers
 from jax.lax import scan
 from jaxsnn.dataset.yinyang import DataLoader, YinYangDataset
 from jaxsnn.functional.lif import LIFParameters
 from jaxsnn.functional.loss import acc_and_loss, nll_loss
 
-# TODO use optax for loss?
 
-
-def train_step(state, batch, get_params_fn, loss_fn, opt_update_fn):
-    opt_state, i = state
-    net_params = get_params_fn(opt_state)
+def train_step(state, batch, loss_fn):
+    opt_state, params, i = state
     input, output = batch
 
     (loss, recording), grads = value_and_grad(loss_fn, has_aux=True)(
-        net_params, (input, output)
+        params, (input, output)
     )
-    opt_state = opt_update_fn(i, grads, opt_state)
-    return (opt_state, i + 1), recording
+    updates, opt_state = optimizer.update(grads, opt_state)
+    params = optax.apply_updates(params, updates)
+    return (opt_state, params, i + 1), recording
 
 
 if __name__ == "__main__":
@@ -57,17 +55,16 @@ if __name__ == "__main__":
     )
 
     rng, init_key = random.split(rng)
-    output_shape, params_initial = snn_init(init_key, input_shape=input_shape)
+    output_shape, params = snn_init(init_key, input_shape=input_shape)
 
-    opt_init, opt_update, get_params = optimizers.adam(step_size)
-    opt_state = opt_init(params_initial)
+    # opt_init, opt_update, get_params = optimizers.adam(step_size)
+    optimizer = optax.adam(step_size)
+    opt_state = optimizer.init(params)
 
     # define functions
     snn_apply = partial(snn_apply, recording=True)
     loss_fn = partial(nll_loss, snn_apply, expected_spikes=expected_spikes)
-    train_step_fn = partial(
-        train_step, get_params_fn=get_params, loss_fn=loss_fn, opt_update_fn=opt_update
-    )
+    train_step_fn = partial(train_step, loss_fn=loss_fn)
 
     overall_time = time.time()
     for epoch in range(epochs):
@@ -76,10 +73,12 @@ if __name__ == "__main__":
 
         # rng, shuffle_key = random.split(rng)
         trainloader = DataLoader(trainset, batch_size, rng=None)
-        (opt_state, i), recording = scan(train_step_fn, (opt_state, 0), trainloader)
+        (opt_state, params, i), recording = scan(
+            train_step_fn, (opt_state, params, 0), trainloader
+        )
         spikes_per_item = jnp.count_nonzero(recording[1]) / len(trainset)
         accuracy, test_loss = acc_and_loss(
-            snn_apply, get_params(opt_state), (test_dataset.vals, test_dataset.classes)
+            snn_apply, params, (test_dataset.vals, test_dataset.classes)
         )
         print(
             f"Epoch: {epoch}, Loss: {test_loss:3f}, Test accuracy: {accuracy:.2f}, Seconds: {time.time() - start:.3f}, Spikes: {spikes_per_item:.1f}"
