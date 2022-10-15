fa1cfbe2785660a8849832d7557c8e263fb9f515
Christian Pehle <christian.pehle@gmail.com>
2022-10-15 14:55:05 +0200

    feat: adex implementation
    Change-Id: I839f6683e85e511742520c4b35224138a54d4a6f

--- jaxsnn/adex.py
+++ jaxsnn/adex.py
@@ -13,16 +13,31 @@ class AdexParameters:
     V_l : ArrayLike
     V_exp : ArrayLike
     C_m_inv : ArrayLike
-    tau_syn_inv : ArrayLike
+    tau_s_inv : ArrayLike
+    v_th : ArrayLike
+    v_reset : ArrayLike
 
 @tree_math.struct
 class AdexState:
     v : ArrayLike
     w : ArrayLike
-    I : ArrayLike
+    s : ArrayLike
 
-def adex_dynamics(s : AdexState, p: AdexParameters):
-    return AdexState(
-        v = (p.g_l * p.C_m_inv) * (p.V_l - s.v) + (p.g_exp * p.C_m_inv) * p.Delta_exp * jnp.exp(1/p.Delta_exp * (s.v - p.V_exp)) - p.C_m_inv * s.w,
-        w = -p.a * p.tau_w_inv * (p.V_l - s.v) - p.tau_w_inv * s.w,
-    )
+def adex_dynamics(p: AdexParameters, gating_function: Callable):
+    def dynamics(s : AdexState):
+        v_dot = (p.g_l * p.C_m_inv) * (p.V_l - s.v) + (p.g_exp * p.C_m_inv) * p.Delta_exp * jnp.exp(1/p.Delta_exp * (s.v - p.V_exp)) - p.C_m_inv * s.w
+        return AdexState(
+            v = v_dot,
+            w = -p.a * p.tau_w_inv * (p.V_l - s.v) - p.tau_w_inv * s.w,
+            s = p.tau_s_inv * (-s.s + gating_function(s.v) * v_dot),
+        )
+    return dynamics
+
+def adex_threshold_projection(p : AdexParameters):
+    def projection(state : AdexState):
+        return AdexState(
+            v = jnp.where(state.v > p.v_th, p.v_reset, state.v),
+            w = jnp.where(state.v > p.v_th, jnp.ones_like(state.w), state.w),
+            s = state.s
+        )
+    return projection
