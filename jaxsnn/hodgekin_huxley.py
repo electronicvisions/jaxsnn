798de09295de40bbc3e53376b7773d85c010aeae
Christian Pehle <christian.pehle@gmail.com>
2022-11-12 21:19:45 +0100

    style: black formatting
    Change-Id: I57e25f2b356d859decf487f86c73053cecd81db5

--- jaxsnn/hodgekin_huxley.py
+++ jaxsnn/hodgekin_huxley.py
@@ -17,12 +17,14 @@ import tree_math
 
 from jaxsnn.base.types import ArrayLike
 
+
 @tree_math.struct
 class HodgekinHuxleyState:
     v: ArrayLike
     m: ArrayLike
     h: ArrayLike
     n: ArrayLike
+
 
 @tree_math.struct
 class HodgekinHuxleyParameters:
@@ -34,48 +36,61 @@ class HodgekinHuxleyParameters:
     E_L: ArrayLike
     C_m_inv: ArrayLike
 
+
 def alpha_n(Vm):
     return (0.02 * (Vm - 25.0)) / (1.0 - np.exp((-1.0 * (Vm - 25.0)) / 9.0))
 
+
 def beta_n(Vm):
     return (-0.002 * (Vm - 25.0)) / (1.0 - np.exp((Vm - 25.0) / 9.0))
- 
+
+
 def alpha_m(Vm):
-    return (0.182*(Vm + 35.0)) / (1.0 - np.exp((-1.0 * (Vm + 35.0)) / 9.0))
+    return (0.182 * (Vm + 35.0)) / (1.0 - np.exp((-1.0 * (Vm + 35.0)) / 9.0))
+
 
 def beta_m(Vm):
     return (-0.124 * (Vm + 35.0)) / (1.0 - np.exp((Vm + 35.0) / 9.0))
- 
+
+
 def alpha_h(Vm):
     return 0.25 * np.exp((-1.0 * (Vm + 90.0)) / 12.0)
 
-def beta_h(Vm): 
+
+def beta_h(Vm):
     return (0.25 * np.exp((Vm + 62.0) / 6.0)) / np.exp((Vm + 90.0) / 12.0)
 
-def I_Na(s : HodgekinHuxleyState, p : HodgekinHuxleyParameters):
-    return p.g_Na * np.power(s.m,3) * s.h * (s.v - p.E_Na)
 
-def I_K(s : HodgekinHuxleyState, p: HodgekinHuxleyParameters):
-    return p.g_K * np.power(s.n,4) * (s.v - p.E_K)
+def I_Na(s: HodgekinHuxleyState, p: HodgekinHuxleyParameters):
+    return p.g_Na * np.power(s.m, 3) * s.h * (s.v - p.E_Na)
 
-def I_L(s : HodgekinHuxleyState, p: HodgekinHuxleyParameters):
+
+def I_K(s: HodgekinHuxleyState, p: HodgekinHuxleyParameters):
+    return p.g_K * np.power(s.n, 4) * (s.v - p.E_K)
+
+
+def I_L(s: HodgekinHuxleyState, p: HodgekinHuxleyParameters):
     return p.g_L * (s.v - p.E_L)
 
+
 def channel_dynamics(alpha, beta):
-    def dynamics(x : ArrayLike, v : ArrayLike):
+    def dynamics(x: ArrayLike, v: ArrayLike):
         return alpha(v) * (1 - x) - beta(v) * x
+
     return dynamics
 
-def hodgekin_huxley_dynamics(p : HodgekinHuxleyParameters):
+
+def hodgekin_huxley_dynamics(p: HodgekinHuxleyParameters):
     m_dynamics = channel_dynamics(alpha_m, beta_m)
     h_dynamics = channel_dynamics(alpha_h, beta_h)
     n_dynamics = channel_dynamics(alpha_n, beta_n)
 
-    def dynamics(s : HodgekinHuxleyState, I : ArrayLike):
+    def dynamics(s: HodgekinHuxleyState, I: ArrayLike):
         return HodgekinHuxleyState(
-            v = -p.C_m_inv * (I_K(s, p) + I_Na(s, p) + I_L(s, p) - I),
-            m = m_dynamics(s.m, s.v),
-            n = n_dynamics(s.n, s.v),
-            h = h_dynamics(s.h, s.v)
+            v=-p.C_m_inv * (I_K(s, p) + I_Na(s, p) + I_L(s, p) - I),
+            m=m_dynamics(s.m, s.v),
+            n=n_dynamics(s.n, s.v),
+            h=h_dynamics(s.h, s.v),
         )
+
     return dynamics
