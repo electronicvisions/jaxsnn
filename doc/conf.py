462fcc235de07224ad1651cab75817f38d8b9d65
Christian Pehle <christian.pehle@gmail.com>
2022-11-17 16:10:44 +0100

    doc: add draft of logo
    Change-Id: Ib671977b7b50484e343d2c879814e564a28e1112

--- doc/conf.py
+++ doc/conf.py
@@ -130,7 +130,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = ""
+html_logo = "_static/logo.png"
 
 html_favicon = ""
 
