6abc96280d7d336d732ad416fcf03d3fa0506656
Christian Pehle <christian.pehle@gmail.com>
2022-11-19 13:43:53 +0100

    refactor: implement queue with standalone methods
    Change-Id: I139309532ccab38712d73a88dda20cfd34b84ffe

--- jaxsnn/event/queue.py
+++ jaxsnn/event/queue.py
@@ -19,45 +19,43 @@ class State:
     used: int
 
 
-class ArrayQueue:
-    @jax.custom_vjp
-    @staticmethod
-    def enqueue(state: State, data: Any) -> State:
-        queue = state.queue.at[(state.head + state.used) % state.queue.shape[0]].set(
-            data
-        )
-        return State(queue=queue, head=state.head, used=state.used + 1)
-
-    @jax.custom_vjp
-    @staticmethod
-    def dequeue(state: State) -> Tuple[Any, State]:
-        x = state.queue[state.head]
-        return (
-            x,
-            State(
-                queue=state.queue.at[state.head].set(jnp.zeros_like(x)),
-                head=(state.head + 1) % state.queue.shape[0],
-                used=state.used - 1,
-            ),
-        )
-
-    @staticmethod
-    def enqueue_fwd(state: State, data: Any):
-        return ArrayQueue.enqueue(state, data), None
-
-    def enqueue_bwd(_, state: State):
-        grad, state = ArrayQueue.dequeue(state)
-        return state, grad
-
-    @staticmethod
-    def dequeue_fwd(state: State):
-        return ArrayQueue.dequeue(state), None
-
-    def dequeue_bwd(_, grad):
-        doutput, state = grad
-        state = ArrayQueue.enqueue(state, doutput)
-        return (state,)
+@jax.custom_vjp
+def enqueue(state: State, data: Any) -> State:
+    queue = state.queue.at[(state.head + state.used) % state.queue.shape[0]].set(data)
+    return State(queue=queue, head=state.head, used=state.used + 1)
 
 
-ArrayQueue.enqueue.defvjp(ArrayQueue.enqueue_fwd, ArrayQueue.enqueue_bwd)
-ArrayQueue.dequeue.defvjp(ArrayQueue.dequeue_fwd, ArrayQueue.dequeue_bwd)
+@jax.custom_vjp
+def dequeue(state: State) -> Tuple[Any, State]:
+    x = state.queue[state.head]
+    return (
+        x,
+        State(
+            queue=state.queue.at[state.head].set(jnp.zeros_like(x)),
+            head=(state.head + 1) % state.queue.shape[0],
+            used=state.used - 1,
+        ),
+    )
+
+
+def enqueue_fwd(state: State, data: Any):
+    return enqueue(state, data), None
+
+
+def enqueue_bwd(_, state: State):
+    grad, state = dequeue(state)
+    return state, grad
+
+
+def dequeue_fwd(state: State):
+    return dequeue(state), None
+
+
+def dequeue_bwd(_, grad):
+    doutput, state = grad
+    state = enqueue(state, doutput)
+    return (state,)
+
+
+enqueue.defvjp(enqueue_fwd, enqueue_bwd)
+dequeue.defvjp(dequeue_fwd, dequeue_bwd)
--- jaxsnn/event/queue_test.py
+++ jaxsnn/event/queue_test.py
@@ -3,7 +3,8 @@
 # Released under Apache 2.0 license as described in the file LICENSE.
 # Authors: Christian Pehle
 
-from jaxsnn.event.queue import ArrayQueue, State
+import jaxsnn.event.queue as queue
+from jaxsnn.event.queue import State
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -13,10 +14,10 @@ def test_enqueue_dequeue():
     state = State(queue=jnp.zeros((100, 2)), head=0, used=0)
 
     for i in range(5):
-        state = ArrayQueue.enqueue(state, jnp.full(2, i))
+        state = queue.enqueue(state, jnp.full(2, i))
 
     for i in range(5):
-        out, state = ArrayQueue.dequeue(state)
+        out, state = queue.dequeue(state)
         np.testing.assert_array_equal(out, jnp.full(2, i))
 
 
@@ -24,7 +25,7 @@ def test_enqueue_has_grad():
     state = State(queue=jnp.zeros((100, 2)), head=0, used=0)
 
     def f(state, x):
-        state = ArrayQueue.enqueue(state, x)
+        state = queue.enqueue(state, x)
         return state.queue[0][0]
 
     grad = jax.grad(f, allow_int=True)(state, np.random.randn(2))
@@ -33,10 +34,10 @@ def test_enqueue_has_grad():
 
 def test_dequeue_has_grad():
     state = State(queue=jnp.zeros((100, 2)), head=0, used=0)
-    state = ArrayQueue.enqueue(state, jnp.full(2, 1.0))
+    state = queue.enqueue(state, jnp.full(2, 1.0))
 
     def f(state):
-        output, state = ArrayQueue.dequeue(state)
+        output, state = queue.dequeue(state)
         return output[0]
 
     grad = jax.grad(f, allow_int=True)(state)
